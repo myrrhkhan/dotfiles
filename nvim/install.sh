995a26278c91c64d3fc74379d913bbe87a7762ec
Myrrh Khan <myrrhkhan@gmail.com>
2023-09-09 02:17:53 -0400

    added an install script

---
+++ nvim/install.sh
@@ -0,0 +1,27 @@
+get_distro()
+{
+    case "$(lsb_release -is)" in
+        Fedora*)    install_cmd="sudo dnf install"
+    esac
+    cmd=${install_cmd}
+}
+
+OS="$(uname -s)"
+case "${OS}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+esac
+
+echo ${machine}
+
+case "${machine}" in
+    Mac*)       install_cmd="brew install";;
+    Linux*)     install_cmd=get_distro;;
+esac
+
+echo ${install_cmd}
+
+command="${install_cmd} neovim"
+echo "Running '${command}'"
+eval " $command"
+
