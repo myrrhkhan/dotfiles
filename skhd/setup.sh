79679de85f73ed111f03397ac5c3c7112f67897b
Myrrh Khan <myrrh@autoreg-4649111.dyn.wpi.edu>
2023-11-05 13:21:42 -0500

    converted bunch of configs to makefiles

--- skhd/setup.sh
+++
@@ -1,5 +0,0 @@
-#/bin/zsh
-
-brew install koekeishiya/formulae/skhd
-
-ln -s ~/dotfiles/skhd/ ~/.config/skhd
--- yabai/yabai-setup.sh
+++
@@ -1,5 +0,0 @@
-#/bin/zsh
-
-brew install koekeishiya/formulae/yabai
-
-ln -s ~/dotfiles/yabai/ ~/.config/yabai
